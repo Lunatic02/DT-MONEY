8358401ffbf079bd5ca423161ee001b0e5f254ae
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-12 20:57:06 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,30 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/Index";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
   return(
     <>
     <Header />
     <Summary />
+    <TransactionsContainer>
+    <TransactionsTable>
+      <tbody>
+        <tr>
+          <td width="50%">Desenvolvimento de site</td>
+          <td><PriceHighlight variant="income">R$ 36.000,00</PriceHighlight></td>
+          <td>Venda</td>
+          <td>30/07/2003</td>
+        </tr>
+        <tr>
+          <td width="50%">Hamburguer</td>
+          <td><PriceHighlight variant="outcome">- R$ 12.000,00</PriceHighlight></td>
+          <td>Venda</td>
+          <td>30/07/2003</td>
+        </tr>
+      </tbody>
+    </TransactionsTable>
+    </TransactionsContainer>
     </>
   )
 }
