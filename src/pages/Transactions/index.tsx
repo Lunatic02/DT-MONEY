1430b5c90a469ad115126fdd7ac429d0344bc58f
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-12 20:29:14 -0300

    Summary

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary/Index";
 
 export function Transactions(){
   return(
     <>
     <Header />
+    <Summary />
     </>
   )
 }
