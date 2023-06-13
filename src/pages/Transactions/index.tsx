e3929c9709e94e68ee1aac8b4f90f7eb578c1a1c
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-13 20:31:28 -0300

    Requisição a Api do json server

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/index";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+  id: number,
+  description: string,
+  type: 'income' | 'outcome',
+  category: string,
+  price: number,
+  createdAt: string
+
+}
+
 export function Transactions(){
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+
+  async function LoadTransactions(){
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(()=>{
+    LoadTransactions()
+  }, [])
+
   return(
     <>
     <Header />
@@ -12,12 +37,17 @@ export function Transactions(){
     <SearchForm />
     <TransactionsTable>
       <tbody>
-        <tr>
-          <td width="50%">Desenvolvimento de site</td>
-          <td><PriceHighlight variant="income">R$ 36.000,00</PriceHighlight></td>
-          <td>Venda</td>
-          <td>30/07/2003</td>
-        </tr>
+        {transactions.map(transaction=>{
+          return(
+            <tr key={transaction.id}>
+              <td width="50%">{transaction.description}</td>
+              <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          )
+        })}
+        
 
       </tbody>
     </TransactionsTable>
