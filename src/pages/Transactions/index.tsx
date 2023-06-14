b98db8456ef644cd3a105eeed3052474fe9a3f3d
Lucas Henrique Torresin da Costa <82097583+Lunatic02@users.noreply.github.com>
2023-06-13 21:19:55 -0300

    Formatter

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary/index";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/Formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -22,9 +23,11 @@ export function Transactions(){
           return(
             <tr key={transaction.id}>
               <td width="50%">{transaction.description}</td>
-              <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+              <td><PriceHighlight variant={transaction.type}>
+                {transaction.type === 'outcome' && '- '}
+                {priceFormatter.format(transaction.price)}</PriceHighlight></td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
             </tr>
           )
         })}
